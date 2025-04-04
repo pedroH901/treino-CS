bb1b99cb5a8f4d98d99e37422646e14b9d1b2d49
pedroH901 <82474105+pedroH901@users.noreply.github.com>
2025-04-04 10:08:12 -0300

    Add files via upload

---
+++ Course/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,12 @@
+﻿namespace Course.Entities.Enums
+{
+    internal class WorkerLevel
+    {
+        enum Workerlevel : int
+        {
+            Junior = 0,
+            MidLevel = 1,
+            Senior = 2
+        }
+    }
+}
